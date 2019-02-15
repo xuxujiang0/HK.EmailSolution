d5361d9f2d3444bc10636975774e336ece400e25
xuxujiang0 <1842125096@qq.com>
2019-02-15 10:49:54 +0800

    新增api项目

---
+++ HK.Email.Api/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HK.Email.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public string Index()
+        {
+            return "Login Index";
+        }
+
+    }
+}
